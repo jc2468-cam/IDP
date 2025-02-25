5dd4acbb445d7a2c9f5673788ea2b4bfcb9eab61
jc2468 <jc2468@cam.ac.uk>
2025-02-25 11:10:39 +0000

    Moved statistic display to seperate file

--- host_src/tacho.py
+++ host_src/tacho.py
@@ -23,16 +23,3 @@ all_ts = times(speeds * steps)
 with open("out.txt", "w") as f:
 	f.write(np.array2string(all_ts, separator=", "))
 print(np.array2string(all_ts, separator=", "))
-
-fig, ax1 = plt.subplots()
-
-for i0 in range(0, speeds * steps, steps):
-	ts = all_ts[i0: i0 + steps]
-	dts = ts[1:] - ts[:-1]
-	mean_t = ts[-1] / ((steps - 1) * 1000)
-	var_t = np.var(dt)
-	print("average freq:", 1 / mean_t)
-	print("variance:", var_t)
-	ax1.scatter(np.linspace(0,1,ts.shape[0]), ts)
-
-plt.show()
