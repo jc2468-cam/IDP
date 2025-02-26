bf7cb8d7ffa783c5c90b21d53934fdf567fa6695
Asmee Mishra <asmeemishra@dhcp-10-248-135-137.eduroam.wireless.private.cam.ac.uk>
2025-02-26 10:25:17 +0000

    follow function

--- src/linefollower.py
+++ src/linefollower.py
@@ -1,27 +1,28 @@
 #1 left, 1 right, 1 middle sensor. 
 
-while True:
-    tank = Sensor()
-    left = tank.readleft()
-    centre = tank.readcentre()
-    right = tank.readright()    
-    #0 is white, 1 is black
+def follow():
+    while True:
+        tank = Sensor()
+        left = tank.readleft()
+        centre = tank.readcentre()
+        right = tank.readright()    
+        #0 is white, 1 is black
 
-    if (left == 0 and centre == 1) and right == 0:
-        tank.forward()
-    elif (left == 0 and centre == 0) and right == 0:
-        tank.stop()
-    elif (left == 1 and centre == 1) and right == 1:
-        tank.stop()
-    elif (left == 0 and centre == 0) and right == 0:
-        tank.stop()
-    elif (left == 1 and centre == 1) and right == 0:
-        tank.turnleft()
-    elif (left == 1 and centre == 0) and right == 0:
-        tank.turnleft()
-    elif (left == 0 and centre == 1) and right == 1:
-        tank.turnright()
-    elif (left == 0 and centre == 0) and right == 1:
-        tank.turnright()
+        if (left == 0 and centre == 1) and right == 0:
+            tank.forward()
+        elif (left == 0 and centre == 0) and right == 0:
+            tank.stop()
+        elif (left == 1 and centre == 1) and right == 1:
+            tank.stop()
+        elif (left == 0 and centre == 0) and right == 0:
+            tank.stop()
+        elif (left == 1 and centre == 1) and right == 0:
+            tank.turnleft()
+        elif (left == 1 and centre == 0) and right == 0:
+            tank.turnleft()
+        elif (left == 0 and centre == 1) and right == 1:
+            tank.turnright()
+        elif (left == 0 and centre == 0) and right == 1:
+            tank.turnright()
 
     
