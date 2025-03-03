a43849a1403542433c2d8fb3c13c087ac14cea5a
jc2468 <jc2468@cam.ac.uk>
2025-03-03 21:22:54 +0000

    Apdated main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
-from interface import *
+from interface.interface import *
 from calibrations import *
-from col_sense import *
 from utime import sleep, ticks_ms
 
 from machine import Pin, PWM, Timer, I2C
+
+if SIM_OUT or SIM_IN:
+    while input().strip() != "Control::start":
+        pass
+    param_command = input().strip()
+    if param_command == "Control::fetch_params":
+        print(f"Control::return_params({WHEEL_DIAM}, {MOTOR_CAL_M}, {MOTOR_CAL_C}, {MOTOR_CAL_T})")
+    else:
+        print("Got param command", param_command)
 
 
 #i2c_bus = I2C(0, sda=Pin(16), scl=Pin(17))
@@ -14,12 +22,19 @@ from machine import Pin, PWM, Timer, I2C
 
 #print('rgb: {}'.format(tcs.read('rgb')))
 
-tank = TrackedTank.default(10.0, 6.5)
-
-tank.drive(0.5)
+tank = TrackedTank.default(AXEL_LENGTH, 6.5)
 
 dt = 0.1
-while True:
+
+tank.drive(0.7)
+for _ in range(20):
     sleep(dt)
     print("pos:", tank.tick(dt))
-
+tank.drive(0.7, 0.02, 0.0)
+for _ in range(20):
+    sleep(dt)
+    print("pos:", tank.tick(dt))
+tank.drive(0.7)
+for _ in range(20):
+    sleep(dt)
+    print("pos:", tank.tick(dt))
