e87c072b2cc55fd4ef0f775ac07aa5b3dca4722f
jc2468 <jc2468@cam.ac.uk>
2025-03-03 21:22:25 +0000

    Added linear regression calculation

--- motor_cal/calc_cal.py
+++ motor_cal/calc_cal.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 
 
@@ -9,14 +10,19 @@ print(file_names)
 
 for f_name in file_names:
 	x = float(f_name.split(".")[0][4:].replace("_", "."))
+	if x > 1.0:
+		break
 	with open(f_name, "r") as f:
 		ts = [int(i) for i in f.read().strip()[1:-1].split(",")]
 	mean_t = (ts[-1] - ts[0]) / ((len(ts) - 1) * 1000)
 	cal_p.append(x)
 	cal_s.append(1/mean_t)
 
+cal_p, cal_s = np.array(cal_p), np.array(cal_s)
+m, c = np.polyfit(cal_p, cal_s, 1)
 print(cal_p)
 print(cal_s)
+print(f"linear fit: s = {m} * p + {c}")
 
 fig, ax = plt.subplots()
 
