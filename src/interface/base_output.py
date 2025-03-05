161b53f528ccdf091a9fcb7c1dd4e3cd88dc7d4f
jc2468 <jc2468@cam.ac.uk>
2025-03-05 15:13:27 +0000

    Switched to telemetry decorators

--- src/interface/base_output.py
+++ src/interface/base_output.py
@@ -1,3 +1,5 @@
+from interface.telemetry_decorator import telemetry_out
+
 from machine import Pin, PWM, Timer
 
 
@@ -12,8 +14,11 @@ class Motor:
         self.pwm_pin.freq(1000)
         self.pwm_pin.duty_u16(0)
         self.vel_scale = vel_scale
+        self.m_id = m_id
+    @telemetry_out(lambda self: f"Motor::off({self.m_id})")
     def off(self):
         self.pwm_pin.duty_u16(0)
+    @telemetry_out(lambda self, velocity: f"Motor::run({self.m_id}, {velocity * self.vel_scale})")
     def run(self, velocity):
         if velocity > 0:
             self.dir_pin.value(0)
@@ -27,14 +32,23 @@ class Led:
         self.pwm_pin = PWM(Pin(pin))
         self.pwm_pin.freq(1000)
         self.state = 0.0
+    @telemetry_out(lambda self, b: f"Led::brightness({self.pwm_pin}, {b})", False)
     def brightness(self, b):
-        self.pwm_pin.duty_u16(int(65535*b))
+        global OUTPUT_MODE
+        if OUTPUT_MODE == 0 or OUTPUT_MODE == 2:
+            self.pwm_pin.duty_u16(int(65535*b))
         self.state = b
+    @telemetry_out(lambda self: f"Led::on({self.pwm_pin})", False)
     def on(self):
-        self.pwm_pin.duty_u16(int(65535))
+        global OUTPUT_MODE
+        if OUTPUT_MODE == 0 or OUTPUT_MODE == 2:
+            self.pwm_pin.duty_u16(int(65535))
         self.state = 1
+    @telemetry_out(lambda self: f"Led::off({self.pwm_pin})", False)
     def off(self):
-        self.pwm_pin.duty_u16(int(0))
+        global OUTPUT_MODE
+        if OUTPUT_MODE == 0 or OUTPUT_MODE == 2:
+            self.pwm_pin.duty_u16(int(0))
         self.state = 0
     def toggle(self):
         self.brightness(1 - self.state)
--- src/interface/telemetry_decorator.py
+++ src/interface/telemetry_decorator.py
@@ -1,41 +1,47 @@
+from config import *
+
 from utime import sleep
 
 
-def telemetry_out(get_telem_str):
-	def wrapper(fn):
-		def inner(*args):
-			global INPUT_MODE
-			if INPUT_MODE == 1 || INPUT_MODE == 2:
-				print(get_telem_str(*args))
-			if INPUT_MODE == 0 || INPUT_MODE == 2:
-				fn(*args)
-		return inner
-	return wrapper
+def telemetry_out(get_telem_str, supress=True):
+    def wrapper(fn):
+        if OUTPUT_MODE == 0:
+            return fn
+        elif OUTPUT_MODE == 1 and supress:
+            return get_telem_str
+        else:
+            def inner(*args):
+                print(get_telem_str(*args))
+                fn(*args)
+            return inner
+    return wrapper
 
 def telemetry_in(indentifier, alt_fn, signal=None, wait=True):
-	def wrapper(fn):
-		def inner(*args):
-			global INPUT_MODE
-			if INPUT_MODE == 1 or INPUT_MODE == 2:
-				if signal != None:
-					print(signal(*args))
-				global sim_command_inputs
-				if not indentifier in sim_command_inputs:
-					sim_command_inputs[indentifier] = [None, False, True]
-				if sim_command_inputs[indentifier][2]:
-					if wait:
-						sim_command_inputs[indentifier][1] = False
-						while not sim_command_inputs[indentifier][1]:
-							sleep(0.05)
-					if sim_command_inputs[indentifier][0] == None:
-						if INPUT_MODE == 1:
-							found = False
-							for _ in range(100):
-								if sim_command_inputs[indentifier][1]:
-									return alt_fn(sim_command_inputs[indentifier][0])
-							raise RuntimeError
-					else:
-						return alt_fn(sim_command_inputs[indentifier][0])
-			return fn(*args)
-		return inner
-	return wrapper
+    def wrapper(fn):
+        if INPUT_MODE == 0:
+            return fn
+        else:
+            force_wait = INPUT_MODE == 1
+            def inner(*args):
+                if signal != None:
+                    print(signal(*args))
+                global sim_command_inputs
+                if not indentifier in sim_command_inputs:
+                    sim_command_inputs[indentifier] = [None, False, True]
+                if sim_command_inputs[indentifier][2]:
+                    if wait:
+                        sim_command_inputs[indentifier][1] = False
+                        while not sim_command_inputs[indentifier][1]:
+                            sleep(0.05)
+                    if sim_command_inputs[indentifier][0] == None:
+                        if force_wait:
+                            found = False
+                            for _ in range(100):
+                                if sim_command_inputs[indentifier][1]:
+                                    return alt_fn(sim_command_inputs[indentifier][0])
+                            raise RuntimeError
+                    else:
+                        return alt_fn(sim_command_inputs[indentifier][0])
+                return fn(*args)
+            return inner
+    return wrapper
