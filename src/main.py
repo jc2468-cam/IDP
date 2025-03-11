89cb1c340d2f60b24e86ab3702a3471a020da310
jc2468 <jc2468@cam.ac.uk>
2025-03-11 15:52:43 +0000

    Tests for actuators and servos

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-from interface.interface import *
 from config import *
+
+from interface.interface import *
+from interface.base_output import *
 from calibrations import *
-from utime import sleep, ticks_ms
 from interface.linefollowing import LineSensor
 
+from utime import sleep, ticks_ms
 from machine import Pin, PWM, Timer, I2C
 
 if AWAIT_LINK:
@@ -23,11 +25,21 @@ if AWAIT_LINK:
 
 #print('rgb: {}'.format(tcs.read('rgb')))
 
-dt = 0.3
-tank = TrackedTank.default(AXLE_LENGTH, 6.5)
-line_sesnors = LineSensor(tank, 9, 10, 11, 12)
-line_sesnors.line_follow(0.45, dt)
-# a comment
+# dt = 0.3
+# tank = TrackedTank.default(AXLE_LENGTH, 6.5)
+# line_sensors = LineSensor(tank, 9, 10, 11, 12)
+# line_sensors.line_follow(0.45, dt)
+
+m0 = Motor(0)
+m0.run(-1)
+
+s1 = Servo(0)
+for _ in range(3):
+    s1.set_position(0.7)
+    sleep(3)
+    s1.set_position(0.0)
+    sleep(3)
+m0.off()
 
 # tank.drive(0.7)
 # for _ in range(20):
