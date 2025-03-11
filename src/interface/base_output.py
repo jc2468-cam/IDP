24a673687a28fc69ea9b7f44a6b1e13901ee1b34
jc2468 <jc2468@cam.ac.uk>
2025-03-11 13:17:45 +0000

    Added servo and actuator code

--- src/interface/base_output.py
+++ src/interface/base_output.py
@@ -58,3 +58,101 @@ class Led:
 
         timer = Timer()
         timer.init(freq=frequency, mode=Timer.PERIODIC, callback=flip_led)
+
+class Actuator:
+    def __init__(m_id):
+        if m_id == 0:
+            self.dir_pin = Pin(4, Pin.OUT)
+            self.pwm_pin = PWM(Pin(5))
+        else:
+            self.dir_pin = Pin(7, Pin.OUT)
+            self.pwm_pin = PWM(Pin(6))
+        self.pwm_pin.freq(1000)
+        self.pwm_pin.duty_u16(0)
+        self.position = None
+        self.runs = 0
+        self.m_id = m_id
+    @telemetry_out(lambda self: f"Actuator::off({self.m_id})")
+    def off(self):
+        self.pwm_pin.duty_u16(0)
+    @telemetry_out(lambda self, velocity: f"Actuator::extend({self.m_id}, {velocity * self.vel_scale})")
+    def extend(self, velocity):
+        if velocity > 0:
+            self.dir_pin.value(0)
+        else:
+            self.dir_pin.value(1)
+            velocity += 1
+        self.pwm_pin.duty_u16(int(65535*velocity*self.vel_scale))
+    @telemetry_out(lambda self, retract=True, timeout=1000: f"Actuator::home({self.m_id}, {retract})")
+    def home(self, retract, timeout):
+        self.dir_pin.value(1 if retract else 0)
+        self.pwm_pin.duty_u16(int(65535))
+
+        def homeing_end(t):
+            self.pwm_pin.duty_u16(0)
+            self.position = 0 if retract else 1
+
+        timer = Timer()
+        timer.init(mode=Timer.ONE_SHOT, period=timeout, callback=homeing_end)
+    def extend_to(self, target, rate=1):
+        full_time = 500
+
+        if self.position == None:
+            raise RuntimeError
+        dist = target - self.position
+
+        if dist != 0:
+            def extension_end(t):
+                self.off()
+                self.position = target
+                self.runs += 1
+
+            vel = rate if dist > 1 else -1
+            timeout = abs(dist) * full_time // rate
+            self.run(vel)
+
+            timer = Timer()
+            timer.init(mode=Timer.ONE_SHOT, period=timeout, callback=extension_end)
+
+class Servo:
+    def __init__(m_id, initial_pos=0):
+        max_duty = 7864
+        min_duty = 1802
+        half_duty = int(max_duty/2)
+        if m_id == 0:
+            self.dir_pin = Pin(4, Pin.OUT)
+            self.pwm_pin = PWM(Pin(5))
+        else:
+            self.dir_pin = Pin(7, Pin.OUT)
+            self.pwm_pin = PWM(Pin(6))
+        self.pwm_pin.freq(1000)
+        self.pwm_pin.duty_u16(0)
+        self.m_id = 0
+    @telemetry_out(lambda self, position: f"Servo::set_position({self.m_id}, {position})")
+    def set_position(self, position):
+        max_duty = 7864
+        min_duty = 1802
+        half_duty = int(max_duty/2)
+        grad = max_duty - min_duty
+        self.pwm_pin.duty_u16(min_duty + int(grad * position))
+
+
+from machine import Pin, PWM
+from time import sleep
+# Set up PWM Pin for servo control
+servo_pin = machine.Pin(15)
+servo = PWM(servo_pin)
+# Set Duty Cycle for Different Angles
+#Set PWM frequency
+frequency = 50
+try:
+ while True:
+ #Servo at 0 degrees
+ servo.duty_u16(min_duty)
+ sleep(2)
+ #Servo at 90 degrees
+ servo.duty_u16(half_duty)
+ sleep(2)
+ #Servo at 180 degrees
+ servo.duty_u16(max_duty)
+ sleep(2) 
