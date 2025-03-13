f10292ac293b670ceb850134e534477f94b7f2e1
jc2468 <jc2468@cam.ac.uk>
2025-03-13 00:15:44 +0000

    Added busy loop mode

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from interface.interface import *
 from interface.base_output import *
 from calibrations import *
 from interface.linefollowing import LineSensor
-from interface.shortestpath import *
+# from interface.shortestpath import *
 
 from utime import sleep, ticks_ms
 from machine import Pin, PWM, Timer, I2C
@@ -31,40 +31,94 @@ if INPUT_MODE == 1 or INPUT_MODE == 2:
 
 #print('rgb: {}'.format(tcs.read('rgb')))
 
+
+
+turns = [1, 1, 1, 0, 1] * 3
+path = [("t", 1), ("t", 0), ("t", -1), ("t", -1), ("t", 1), ("t", 1)]
+pos = 0
+
 dt = 0.2
+v_f = 0.8
 tank = TrackedTank.default(AXLE_LENGTH, 6.5)
-line_sensors = LineSensor(tank, 9, 10, 11, 12)
-line_sensors.line_follow(0.8, dt)
-line_sensors.find_path(4, 62)
 
-"""def next_junction(t):
-    print("hello")
-    global enabled
-    global line_sensors, pos, turns
-    
-    def reactivate(t):
-        global enabled, line_sensors
-        enabled = True
-        line_sensors.bending = False
+MODE = 1
+if MODE == 0:
+    line_sensors = LineSensor(tank, 9, 10, 11, 12)
+    line_sensors.line_follow(v_f, dt)
+    line_sensors.find_path(4, 62)
 
-        line_sensors.line_follow(0.8, dt)
-    
-    if enabled:
-        enabled = False
-        if turns[pos] != 0:
-            line_sensors.bending = True
-            #sleep(0.2)
-            line_sensors.stop_follow()
-            sleep(0.3)
-            print("turning")
-            line_sensors.tank.spin(turns[pos] * 0.75)
-            timer = Timer()
-            timer.init(mode=Timer.ONE_SHOT, period=950, callback=reactivate)
-        else:
-            print("skipping")
-            sleep(0.2)
+    """def next_junction(t):
+        print("hello")
+        global enabled
+        global line_sensors, pos, turns
+        
+        def reactivate(t):
+            global enabled, line_sensors
             enabled = True
-        pos += 1"""
+            line_sensors.bending = False
 
-line_sensors.sensor1.bind_interupt(line_sensors.next_junction, 1)
-line_sensors.sensor4.bind_interupt(line_sensors.next_junction, 1)
+            line_sensors.line_follow(v_f, dt)
+        
+        if enabled:
+            enabled = False
+            if turns[pos] != 0:
+                line_sensors.bending = True
+                #sleep(0.2)
+                line_sensors.stop_follow()
+                sleep(0.3)
+                print("turning")
+                line_sensors.tank.spin(turns[pos] * 0.75)
+                timer = Timer()
+                timer.init(mode=Timer.ONE_SHOT, period=950, callback=reactivate)
+            else:
+                print("skipping")
+                sleep(0.2)
+                enabled = True
+            pos += 1"""
+
+    line_sensors.sensor1.bind_interupt(line_sensors.next_junction, 1)
+    line_sensors.sensor4.bind_interupt(line_sensors.next_junction, 1)
+elif MODE == 1:
+    sensor1 = DigitalInput(9)
+    sensor2 = DigitalInput(10)
+    sensor3 = DigitalInput(11)
+    sensor4 = DigitalInput(12)
+    while True:
+        while sensor1.value() == 0 and sensor4.value() == 0:
+            v_r = 0.017 * (sensor2.value() - sensor3.value())
+            if OUTPUT_MODE == 1 or OUTPUT_MODE == 2:
+                print("pos:", tank.tick(dt))
+            tank.drive(v_f, v_r)
+            sleep(dt)
+        print("pos:", tank.tick(dt))
+        command = path[pos][0]
+        value = path[pos][1]
+        print("Junction Detected", command, value)
+        if command == "t":
+            sleep(0.3)
+            print("pos:", tank.tick(0.3))
+            if value != 0:
+                tank.spin(value * 0.75)
+                print("Spinning")
+                sleep(0.95)
+                print("pos:", tank.tick(0.95))
+        elif command == "r":
+            tank.drive(-v_f)
+            sleep(1.25)
+            print("pos:", tank.tick(1.25))
+            tank.spin(1)
+            sleep(1.5)
+            print("pos:", tank.tick(1.5))
+            tank.stop()
+        sleep(2)
+        pos += 1
+        if pos == len(path):
+            tank.stop()
+            break
+    while True:
+        pass
+elif MODE == 2:
+    tank.drive(0.6, -0.02)
+    for _ in range(50):
+        print("pos:", tank.tick(dt))
+        sleep(dt)
